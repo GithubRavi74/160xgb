0ae860e9a64f83a29425be88f42091b8ed465662
Kiran Tang <153486303+GithubRavi74@users.noreply.github.com>
2026-03-05 16:50:53 +0800

    Update 10_brooding.py

--- pages/10_brooding.py
+++ pages/10_brooding.py
@@ -180,7 +180,8 @@ if st.button("🚀 Analyze Brooding Period Impact On Harvest Weight"):
     # -------------------------------------------------
     # HARVEST IMPACT FORECAST
     # -------------------------------------------------
-    st.markdown(" \n") st.markdown(" \n")
+    st.markdown(" \n") 
+    st.markdown(" \n")
     st.subheader("🚀 Day 35 Harvest Impact Forecast")
 
     c1, c2, c3 = st.columns(3)
